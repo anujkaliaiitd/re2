55487b80d80967cb1fc87c285bea1392d2843886
Anuj Kalia <anujkaliaiitd@gmail.com>
2019-01-08 18:59:00 -0500

    save work on regex state analysis

--- akalia/regex_analysis.cc
+++ akalia/regex_analysis.cc
@@ -4,18 +4,40 @@
 
 #include <stdint.h>
 #include <string>
-#include <thread>
-#include <vector>
+#include <fstream>
 
 #include "re2/prog.h"
 #include "re2/re2.h"
 #include "re2/regexp.h"
 
-int main() {
-  std::string s = "a";
-  re2::Regexp *re = re2::Regexp::Parse(s, re2::Regexp::LikePerl, NULL);
+size_t get_num_states(std::string regex) {
+  re2::Regexp *re = re2::Regexp::Parse(regex, re2::Regexp::LikePerl, NULL);
+  if (re == nullptr) {
+    printf("failed to parse regex %s\n", regex.c_str());
+    return 0;
+  }
   re2::Prog *prog = re->CompileToProg(0);
+  if (prog == nullptr) {
+    printf("failed to compile regex %s\n", regex.c_str());
+    return 0;
+  }
 
-  size_t num_states = prog->BuildEntireDFA(re2::Prog::kFirstMatch, nullptr);
-  printf("Anuj: num_states = %zu\n", num_states);
+  return prog->BuildEntireDFA(re2::Prog::kFirstMatch, nullptr);
+}
+
+int main() {
+  assert(get_num_states("a") == 3);
+  assert(get_num_states("ab") == 4);
+  assert(get_num_states("(a|b)*abb(a|b)*") == 10);
+
+  std::ifstream in("akalia/dataset.txt");
+
+  std::string s;
+  s.reserve(1000);  // For performance
+
+  while (true) {
+    std::getline(in, s);
+    if (s.empty()) break;
+    printf("regex %s, states %zu\n", s.c_str(), get_num_states(s));
+  }
 }
