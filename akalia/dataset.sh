3c3d7008d3113a3f72953f2d2132e6f1efe1d541
Anuj Kalia <anujkaliaiitd@gmail.com>
2019-01-09 17:52:29 -0500

    save

--- akalia/dataset.sh
+++ akalia/dataset.sh
@@ -1,9 +1,12 @@
-filenames="emails.txt uris.txt numbers.txt strings.txt dates.txt misc.txt address.txt"
+filenames="emails.txt uris.txt numbers.txt strings.txt dates.txt misc.txt address.txt markup.txt"
 rm $filenames
 
 # dataset.txt contains the full dataset
 rm dataset.txt
 touch dataset.txt
+
+# For some regex categories, the regexlib website contains more than 100
+# entries, which require fetching two pages.
 
 # Emails
 echo "Downloading email regexes"
@@ -45,6 +48,10 @@ wget -O address2.txt "http://regexlib.com/Search.aspx?k=&c=7&m=-1&ps=100&p=2"
 cat address2.txt >> address.txt
 rm address2.txt
 
+# Markup
+echo "Downloading markup regexes"
+wget -O markup.txt "http://regexlib.com/Search.aspx?k=&c=8&m=-1&ps=100"
+
 for file in $filenames; do
   echo "Post-processing $file"
 	# Extract only lines with the regexes
@@ -60,21 +67,36 @@ for file in $filenames; do
 	# Remove the trailing HTML tags
 	sed -i 's/<\/div><\/td>//' $file
 
-  # Some HTML sequences require multiple calls to recode.
-  # Example: &amp;lt -> &lt -> <
-  # Just run it a bunch of times
-  #while read line; do
-  #  echo "Recoding $line"
-  #  echo $line | recode html..ascii
-  #done < $file
+  # Recode the file from HTML to ASCII. Some HTML sequences require multiple
+  # calls to recode (e.g., "&amp;lt" becomes &lt becomes "<"), so we just run it
+  # a bunch of times.
 
-  #recode html..ascii $file
-  #recode html..ascii $file
-  #recode html..ascii $file
-  #recode html..ascii $file
-  #recode html..ascii $file
+  # file_decoded is the decoded version of $file
+  rm -f file_decoded
+  touch file_decoded
 
+  while read line; do
+    # `recode` fails for the entire file if there's an ill-formatted line.
+    # (This failure typically happens due to encoded UTF8 characters.)
+    # To work around this limitation of recode, we recode $file line-by-line by
+    # using tmpfile as a single-line file.
+    rm -f tmpfile
+    echo $line > tmpfile
+
+    recode -q html..ascii tmpfile
+    recode -q html..ascii tmpfile
+    recode -q html..ascii tmpfile
+    if recode -q html..ascii tmpfile; then
+      cat tmpfile >> file_decoded
+    else
+      echo "recode: Failed to decode $line"
+    fi
+  done < $file
+
+  mv file_decoded $file
   cat $file >> dataset.txt
 done
+
+rm tmpfile
 
 
